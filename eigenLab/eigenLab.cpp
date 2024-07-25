d681330078ce017f1f4a67f0958017a62da08142
lidegao <lidegao@creality.com>
2024-07-25 15:51:46 +0800

    添加项目文件。

---
+++ eigenLab/eigenLab.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Eigen/Dense>
+int main()
+{
+    Eigen::VectorXi v = Eigen::VectorXi::Zero(5);
+    v(0) = 1;
+    v(1) = 2;
+    v(2) = 3;
+    v(3) = 4;
+    v(4) = 5;
+    auto rst = v.array() > 3;
+    auto selectTrue = rst.array().select(v, 0);
+    auto selectFalse = (!rst).array().select(v, 0);
+    std::cout << v << std::endl;
+    std::cout << rst << std::endl;
+    std::cout << selectTrue << std::endl;
+    std::cout << selectFalse << std::endl;
+}
